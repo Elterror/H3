d1dcc21ef6e0eda0d41a038ccc1ad7c674916631
Elias <Reiter.Elias@sus.htl-grieskirchen.at>
2020-10-09 16:46:11 +0200

    streams

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,25 +2,64 @@ import java.io.*;
 import java.nio.file.Files;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Objects;
+import java.util.function.IntPredicate;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Main {
     static List<Weapon> list = new LinkedList<>();
     public static void main(String[] args) throws IOException {
 
+        Main m= new Main();
 
-        /*try(BufferedReader br = new BufferedReader(new FileReader("weapons.csv"))){
-            String line = br.readLine();
-            while(line !=null){
-                String[] arr = line.split(";");
-                list.add(new Weapon(arr[0], CombatType.valueOf(arr[1]),DamageType.valueOf(arr[2]), Integer.parseInt(arr[3]),Integer.parseInt(arr[4]), Integer.parseInt(arr[5]),Integer.parseInt(arr[6])));
-                line = br.readLine();
-            }
+        m.einlesenMitStream();
+        m.printInTable();
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }*/
 
+
+
+       Streams streams = new Streams();
+
+
+
+    }
+
+    public void printInTable(){
+
+        System.out.printf("%-20s", " | " + "Name");
+        System.out.printf("%-9s", " | " + "CombatType");
+        System.out.printf("%-11s ", " | " + "DamageType");
+        System.out.printf("%-6s", " | " + "Damage");
+        System.out.printf("%-3s", " | " + "Speed");
+        System.out.printf("%-6s", " | " + "Lenghth");
+        System.out.printf("%-7s %n", " | " + "Value");
+        System.out.println("+---------------------------------------------------------------------------------+");
+
+        Printable tablePrint = (w) -> w.forEach(t ->{
+            System.out.printf("%-20s", " | " + t.getName());
+            System.out.printf("%-13s", " | " + t.getCombatType());
+            System.out.printf("%-13s ", " | " + t.getDamageType());
+            System.out.printf("%-9s", " | " + t.getDamage());
+            System.out.printf("%-8s", " | " + t.getSpeed());
+            System.out.printf("%-10s", " | " + t.getMinStrength());
+            System.out.printf("%-7s %n", " | " + t.getValue());
+            System.out.println("+---------------------------------------------------------------------------------+");
+        });
+        tablePrint.print(list);
+    }
+
+    public void listSortedByDamage(){
+        listSortedByDamage(list);
+    }
+
+    public void listMitPrintable(){
+        Printable printable = (w -> w.forEach(t -> System.out.println(t.toString())));
+        printable.print(list);
+    }
+
+    public void einlesenMitStream() throws IOException {
         list = Files.lines(new File("weapons.csv").toPath())
                 .skip(1)
                 .map(s -> s.split(";"))
@@ -34,24 +73,24 @@ public class Main {
                         Integer.parseInt(s[6])
                 ))
                 .collect(Collectors.toList());
+    }
 
-        //
+    public void foreachAusgeben(){
+        list.forEach(System.out::println);
+    }
 
+    public void einlesenohneStream(){
+         try(BufferedReader br = new BufferedReader(new FileReader("weapons.csv"))){
+            String line = br.readLine();
+            while(line !=null){
+                String[] arr = line.split(";");
+                list.add(new Weapon(arr[0], CombatType.valueOf(arr[1]),DamageType.valueOf(arr[2]), Integer.parseInt(arr[3]),Integer.parseInt(arr[4]), Integer.parseInt(arr[5]),Integer.parseInt(arr[6])));
+                line = br.readLine();
+            }
 
-
-        //list.forEach(System.out::println);
-
-        /*list.forEach(w ->
-            System.out.println(w.toString()));*/
-        listSortedByDamage(list);
-       Printable printable = (w -> w.forEach(t -> System.out.println(t.toString2())));
-       printable.print(list);
-
-
-       Streams streams = new Streams();
-
-
-
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static List<Weapon> listSortedByDamage(List<Weapon> l){
@@ -65,5 +104,21 @@ public class Main {
             String weapon2 = w2.getCombatType().toString() + w2.getDamageType().toString() + w2.getName();
             return weapon1.compareTo(weapon2);});
         return l;
+    }
+
+    public void aufgabeVier() {
+        final int result = IntStream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).filter((s) -> s % 2 == 1).map((s) -> s * s).sum();
+        System.out.println(result);
+    }
+
+    public void aufgabeDrei(List<Integer> intList, List<String> stringList){
+        final Predicate<Integer> isEven =e -> e % 2 == 0;
+        final Predicate<Integer> isPositive = e-> e>0;
+        final Predicate<Integer> isNull = Objects::isNull;
+        final Predicate<Integer> is0 = e-> e==0;
+        final Predicate<String> isShortWord = e-> e.length()<4;
+
+        intList.stream().filter(isEven.and(isPositive));
+        intList.stream().filter(isEven.negate().and(isPositive));
     }
 }
--- src/main/java/Streams.java
+++ src/main/java/Streams.java
@@ -1,72 +1,82 @@
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class Streams {
 
-    int[] array = new int[1000];
+    int[] array = new int[10000];
 
-    String zufallsString;
+
+    String[] stringArr = new String[10];
     public Streams(){
         for (int i = 0; i < array.length; i++) {
             array[i] = (int) (Math.random()*100 +1);
         }
 
 
+        String abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
         for (int i = 0; i < 10; i++) {
-            zufallsString += (char) (Math.random()*122 +30);
+            String zufallsString = "";
+            for (int j = 0; j < 10; j++) {
+                zufallsString += abc.charAt((int)(Math.random()*48+1));
+            }
+            stringArr[i] = zufallsString;
         }
 
-        System.out.println(zufallsString);
 
     }
 
 
     public double average(int[] numbers) {
-        return Arrays.stream(array).average().orElse(0);
+        return Arrays.stream(numbers).average().orElse(0);
     }
     
     public List<String> upperCase(String[] strings) {
-        return null;
+        return Arrays.stream(strings).map(m -> m.toUpperCase()).collect(Collectors.toList());
     }
     
     public Weapon findWeaponWithLowestDamage(List<Weapon> weapons) {
-        return null;
+        return weapons.stream().min(Comparator.comparing(Weapon::getDamage)).orElse(null);
     }
     
     public Weapon findWeaponWithHighestStrength(List<Weapon> weapons) {
-        return null;
+        return weapons.stream().max(Comparator.comparing(Weapon::getMinStrength)).orElse(null);
     }
     
     public List<Weapon> collectMissileWeapons(List<Weapon> weapons) {
-        return null;
+        return weapons.stream().filter(w-> w.getDamageType()==DamageType.MISSILE).collect(Collectors.toList());
     }
     
     public Weapon findWeaponWithLongestName(List<Weapon> weapons) {
-        return null;
+         return weapons.stream().max((o1, o2) -> o1.getName().length()-o2.getName().length()).orElse(null);
     }
     
     public List<String> toNameList(List<Weapon> weapons) {
-        return null;
+        return weapons.stream().map(weapon -> weapon.getName()).collect(Collectors.toList());
     }
     
     public int[] toSpeedArray(List<Weapon> weapons) {
-        return null;
+         return weapons.stream().mapToInt(weapon -> weapon.getSpeed()).toArray();
+
     }
     
     public int sumUpValues(List<Weapon> weapons) {
-        return 0;
+        return weapons.stream().mapToInt(value -> value.getValue()).sum();
     }
     
     public long sumUpHashCodes(List<Weapon> weapons) {
-        return 0;
+        return weapons.stream().mapToInt(value -> value.hashCode()).sum();
     }
     
     public List<Weapon> removeDuplicates(List<Weapon> weapons) {
-        return null;
+        return weapons.stream().map(weapon -> weapon).collect(Collectors.toList());
     }
     
     public void increaseValuesByTenPercent(List<Weapon> weapons) {
-       //implement this
+       weapons.stream().forEach(v -> v.setValue((int) (v.getValue()*1.1)));
     }
 }
